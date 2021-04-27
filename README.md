6f5c6cd4c6915ef8f6e30619ccfecc05b9a5c738
Ana Carolina Ferreira <souzacarol451@gmail.com>
2021-04-26 21:02:33 -0300

    img readme

---
+++ README.md
@@ -0,0 +1,2 @@
+# Be-The-Hero
+Aplicação completa(Back-End, Front-End e Mobile feita com Node, React) que visa conectar ONGs com pessoas/heróis que podem ajudar em suas causas. Projeto feito durante a semana OmniStack 11.0 - Rocketseat.
---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,26 @@
+const generateUniqueId = require('../utils/generateUniqueId');
+const connection = require('../database/connection');
+
+module.exports={
+async index (require, response){
+    const ongs = await connection('ongs').select('*');
+
+return response.json(ongs);
+},
+
+    async create(request, response){
+        const {name, email, whatsapp, city, uf} = request.body;
+
+        const id = generateUniqueId();
+
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+            })
+            return response.json({id});
+    }
+};
